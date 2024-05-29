71de294622cdb63f215d85746931a9fc3b02931b
Tor Andersson <tor@ccxvii.net>
2024-05-29 23:47:50 +0200

    OO rules do not apply austrian theater scenario

--- rules.js
+++ rules.js
@@ -414,7 +414,7 @@ make_protect(P_HANOVER, data.country.Hanover)
 make_protect(P_AUSTRIA, data.country.Austria)
 
 function is_conquest_space(pow, s) {
-	if (pow === P_PRUSSIA) {
+	if (pow === P_PRUSSIA && game.scenario >= 3) {
 		if (is_offensive_option() || game.turn < 3)
 			return set_has(primary_objective[pow], s)
 		return false
